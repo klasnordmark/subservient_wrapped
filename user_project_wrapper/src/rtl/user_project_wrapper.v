1332c15369afb8ce781fc8ce8c3c2bf9a8a7a020
Klas Nordmark <klas.nordmark.se@ieee.org>
2021-04-29 13:19:30 +0200

    Cores for wrapping together Subservient with an SRAM macro, and another one for adding it to the project area.

--- user_project_wrapper/src/rtl/user_project_wrapper.v
+++ user_project_wrapper/src/rtl/user_project_wrapper.v
@@ -75,22 +75,13 @@ module user_project_wrapper #(
     input   user_clock2
 );
 
+
+
     /*--------------------------------------*/
     /* User project is instantiated  here   */
     /*--------------------------------------*/
 
-    user_proj_example mprj (
-    `ifdef USE_POWER_PINS
-	.vdda1(vdda1),	// User area 1 3.3V power
-	.vdda2(vdda2),	// User area 2 3.3V power
-	.vssa1(vssa1),	// User area 1 analog ground
-	.vssa2(vssa2),	// User area 2 analog ground
-	.vccd1(vccd1),	// User area 1 1.8V power
-	.vccd2(vccd2),	// User area 2 1.8V power
-	.vssd1(vssd1),	// User area 1 digital ground
-	.vssd2(vssd2),	// User area 2 digital ground
-    `endif
-
+    subservient_wrapped subservient_inst (
 	// MGMT core clock and reset
 
     	.wb_clk_i(wb_clk_i),
